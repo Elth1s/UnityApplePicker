d4e35ba8152e90032b2e6bf6c25c4333abb05bbc
Elthis <dg646726@gmail.com>
2022-05-20 10:16:13 +0300

    Fixe cursor visible

--- Assets/Scripts/EscapePressed.cs
+++ Assets/Scripts/EscapePressed.cs
@@ -6,6 +6,9 @@ public class EscapePressed : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
             SceneManager.LoadScene("_Scene_Begin");
+        }
     }
 }
