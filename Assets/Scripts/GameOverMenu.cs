a6716df257246e9b18f8c90751507ba0ae780a1b
Elthis <dg646726@gmail.com>
2022-05-20 10:00:53 +0300

    Fixe cursor

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -3,8 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
     public void PlayAgain()
     {
+
         SceneManager.LoadScene("_Scene_0");
     }
 }
